509590f81ee16a8adf1b7ed1e843fed4b6c8124e
Anush <anushudupi6921@gmail.com>
2024-04-03 10:18:20 +0530

    disable Timer2

--- USB_Driver_test/User/UART/UART.c
+++ USB_Driver_test/User/UART/UART.c
@@ -35,7 +35,7 @@ uint8_t RCC_Configuration( void )
 {
     RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA, ENABLE );
     RCC_APB1PeriphClockCmd( RCC_APB1Periph_USART2, ENABLE );
-    RCC_APB1PeriphClockCmd( RCC_APB1Periph_TIM2, ENABLE );
+//    RCC_APB1PeriphClockCmd( RCC_APB1Periph_TIM2, ENABLE );
     RCC_AHBPeriphClockCmd( RCC_AHBPeriph_DMA1, ENABLE );
     return 0;
 }
